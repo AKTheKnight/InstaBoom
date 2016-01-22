d6d5e819f29dba6939028c0bcf5f8f3aef2284f5
AKTheKnight <AKTheKnight.minecraft@gmail.com>
2016-01-22 10:37:56 +0000

    The real 1.0.1 update. Fixed issues. Working now

--- src/main/java/com/aktheknight/instaboom/InstaBoom.java
+++ src/main/java/com/aktheknight/instaboom/InstaBoom.java
@@ -16,6 +16,8 @@ public class InstaBoom {
 	public static final String MODNAME = "InstaBoom";
 	public static final String VERSION = "1.8.9-1.0.0";
 	
+	@Mod.Instance
+	public static InstaBoom instance;
 	
 	public static final Logger LOGGER = LogManager.getLogger(MODID);
 	
