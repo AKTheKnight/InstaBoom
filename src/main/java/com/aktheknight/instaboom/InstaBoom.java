633670b03f9e745c21342468f88037c6162d5dab
AKTheKnight <AKTheKnight.minecraft@gmail.com>
2016-02-01 21:43:11 +0000

    Mod not required client side

--- src/main/java/com/aktheknight/instaboom/InstaBoom.java
+++ src/main/java/com/aktheknight/instaboom/InstaBoom.java
@@ -9,12 +9,12 @@ import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.fml.common.Mod.EventHandler;
 import net.minecraftforge.fml.common.event.FMLPreInitializationEvent;
 
-@Mod(modid=InstaBoom.MODID, name=InstaBoom.MODNAME, version=InstaBoom.VERSION)
+@Mod(modid=InstaBoom.MODID, name=InstaBoom.MODNAME, version=InstaBoom.VERSION, acceptableRemoteVersions = "*")
 
 public class InstaBoom {
 	public static final String MODID = "InstaBoom";
 	public static final String MODNAME = "InstaBoom";
-	public static final String VERSION = "1.8.9-1.0.2";
+	public static final String VERSION = "1.8.9-1.0.3";
 	
 	@Mod.Instance
 	public static InstaBoom instance;
